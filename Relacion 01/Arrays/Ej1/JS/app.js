f7e777041e8102fe0b69451eed3fa72414378b66
Agustin <agustind1212@gmail.com>
2025-01-13 01:05:38 +0100

    avances en guis de ejercicios

--- Relacion 01/Arrays/Ej1/JS/app.js
+++ Relacion 01/Arrays/Ej1/JS/app.js
@@ -4,8 +4,25 @@
     Nota: La suma parcial de un elemento del array será la suma de ese elemento y todos los anteriores a él.
     Ej: Para el array [1,2,3,4,5,6], el array resultante será [1,3,6,10,15,21] */
 
-    function calcular(arrayValores){
+    //Variable global para almacenar el array de valores
+    let arrayValores = [];
+
+
+    function pedirNumeros(){
+        let vector = prompt("Dime numeros enteros separados por comas");
     
+        arrayValores = vector.split(',').map(Number);
+
+        if(arrayValores.some(isNaN)){
+            alert("Por favor ingresa solo numeros enteros separados por comas.")
+            return null; //sale de la funcion si hay valores invalidos
+        }
+        
+    }
+
+    function calcular(arrayValores){
+
+        
         let suma = 0;
         let arraySuma = [];
 
---
+++ Relacion 01/Arrays/Ej2/JS/app.js
@@ -0,0 +1,15 @@
+let X;
+let Y;
+
+function pedirNumeros(){
+    X = prompt("Dime el numero (entero positivo) X");
+    Y = prompt("Dime EL NUMERO (entero positivo) Y");
+}
+
+function countBy(X ,Y){
+    
+    if(X < 0 || Y < 0){
+        let respuesta = [];
+    }
+    return respuesta;
+}
